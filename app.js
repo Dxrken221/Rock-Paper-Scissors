85f75c7146ac7e84b63e751c06f3319977223c70
dxrken <132681496+Dxrken221@users.noreply.github.com>
2024-10-04 14:03:23 +0530

    final functions done!

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let computerChoice;
 let humanChoice;
 let humanScore = 0;
 let computerScore = 0;
+let result;
 
 function getComputerChoice() {
     computerChoice= Math.random();
@@ -26,18 +27,34 @@ function getHumanChoice() {
 function playRound(humanChoice, computerChoice) {
     if ((humanChoice==='rock' && computerChoice==='paper') || (humanChoice==='paper' && computerChoice==='scissors') || (humanChoice==='scissors' && computerChoice==='rock')) {
         console.log("You loose! "+ computerChoice +" beats "+ humanChoice );
+        result = "loss"
     }
     else if ((computerChoice==='rock' && humanChoice==='paper') || (computerChoice==='paper' && humanChoice==='scissors') || (computerChoice==='scissors' && humanChoice==='rock')) {
         console.log("You win! "+humanChoice+" beats "+computerChoice);
+        result = "win"
     }
     else {
         console.log("Round tied!");
+        result = "draw"
     }
+    if (result === "win"){
+        humanScore = humanScore + 1;
+        return humanScore;
+    }
+    else if (result === "loss"){
+        computerScore = computerScore + 1;
+        return computerScore;
+    }
+}
 
-} 
 
-humanChoice = getHumanChoice();
-computerChoice = getComputerChoice();
-playRound(humanChoice, computerChoice);
+for (let i=0; i<5; i++) {
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+}
+
+console.log("Your score = " +humanScore);
+console.log("Computer's score = "+computerScore);
 
  
